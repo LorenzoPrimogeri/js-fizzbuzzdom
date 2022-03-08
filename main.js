9474ee868560b41d598386c33f76677f94fa182d
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-08 15:47:42 +0100

    Complete full responsive

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let index = 0;
-const divSquares = document.querySelector(".row");
+const divSquares = document.querySelector(".container");
 for (index = 1; index <= 100; index++) {
     if (index % 3 == 0 && index % 5 == 0) {
-        const element = `<div class="fizzBuzz square d-flex justify-content-center align-items-center box">Fizzbuzz</div>`;
+        const element = `<div class="fizzBuzz d-flex justify-content-center align-items-center box">Fizzbuzz</div>`;
         divSquares.innerHTML += element;
     } else if (index % 3 == 0) {
-        const element = `<div class="fizz square d-flex justify-content-center align-items-center box">Fizz</div>`;
+        const element = `<div class="fizz d-flex justify-content-center align-items-center box">Fizz</div>`;
         divSquares.innerHTML += element;
     } else if (index % 5 == 0) {
-        const element = `<div class="buzz square d-flex justify-content-center align-items-center box">buzz</div>`;
+        const element = `<div class="buzz d-flex justify-content-center align-items-center box">buzz</div>`;
         divSquares.innerHTML += element;
-    } else {
-        console.log(index)
-        const element = `<div class="square d-flex justify-content-center align-items-center normalBg box">${index}</div>`;
+    } else{
+        const element = `<div class="d-flex justify-content-center align-items-center normalBg box">${index}</div>`;
         divSquares.innerHTML += element;
     }
 }
