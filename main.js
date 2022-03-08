75229a327e4ddf15f66a4d5315f1bcb35abbd2cb
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-08 15:04:19 +0100

    exercise complete

---
+++ main.js
@@ -0,0 +1,18 @@
+let index = 0;
+const divSquares = document.querySelector(".row");
+for (index = 1; index <= 100; index++) {
+    if (index % 3 == 0 && index % 5 == 0) {
+        const element = `<div class="fizzBuzz square d-flex justify-content-center align-items-center ">Fizzbuzz</div>`;
+        divSquares.innerHTML += element;
+    } else if (index % 3 == 0) {
+        const element = `<div class="fizz square d-flex justify-content-center align-items-center">Fizz</div>`;
+        divSquares.innerHTML += element;
+    } else if (index % 5 == 0) {
+        const element = `<div class="buzz square d-flex justify-content-center align-items-center">buzz</div>`;
+        divSquares.innerHTML += element;
+    } else {
+        console.log(index)
+        const element = `<div class="square d-flex justify-content-center align-items-center">${index}</div>`;
+        divSquares.innerHTML += element;
+    }
+}
