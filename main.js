35e55bb77944c7d4ad2416394b7c4ebba7d00640
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-08 15:09:04 +0100

    Little change on css

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ let index = 0;
 const divSquares = document.querySelector(".row");
 for (index = 1; index <= 100; index++) {
     if (index % 3 == 0 && index % 5 == 0) {
-        const element = `<div class="fizzBuzz square d-flex justify-content-center align-items-center ">Fizzbuzz</div>`;
+        const element = `<div class="fizzBuzz square d-flex justify-content-center align-items-center box">Fizzbuzz</div>`;
         divSquares.innerHTML += element;
     } else if (index % 3 == 0) {
-        const element = `<div class="fizz square d-flex justify-content-center align-items-center">Fizz</div>`;
+        const element = `<div class="fizz square d-flex justify-content-center align-items-center box">Fizz</div>`;
         divSquares.innerHTML += element;
     } else if (index % 5 == 0) {
-        const element = `<div class="buzz square d-flex justify-content-center align-items-center">buzz</div>`;
+        const element = `<div class="buzz square d-flex justify-content-center align-items-center box">buzz</div>`;
         divSquares.innerHTML += element;
     } else {
         console.log(index)
-        const element = `<div class="square d-flex justify-content-center align-items-center">${index}</div>`;
+        const element = `<div class="square d-flex justify-content-center align-items-center normalBg box">${index}</div>`;
         divSquares.innerHTML += element;
     }
 }
